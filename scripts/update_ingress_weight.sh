3117089d3e25916919df1a78db20cdcceddc9897
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-06-07 17:39:40 +0530

    Update update_ingress_weight.sh

--- scripts/update_ingress_weight.sh
+++ scripts/update_ingress_weight.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
+
 SERVICE=$1
 NEW_WEIGHT=$2
+OLD_WEIGHT=$((100 - NEW_WEIGHT))
 
-cat <<EOF | kubectl apply -f -
+cat <<EOM | kubectl apply -f -
 apiVersion: networking.k8s.io/v1
 kind: Ingress
 metadata:
-  name: traffic-split1
+  name: traffic-split
+  namespace: default
   annotations:
     alb.ingress.kubernetes.io/group.name: traffic-split
     alb.ingress.kubernetes.io/load-balancer-name: traffic-split
-    #alb.ingress.kubernetes.io/security-groups: sg-00f9cbfa237460027
     alb.ingress.kubernetes.io/scheme: internet-facing
-    alb.ingress.kubernetes.io/actions.weighted-canary: |
+    alb.ingress.kubernetes.io/target-type: ip
+    alb.ingress.kubernetes.io/actions.canary: |
       {
         "type": "forward",
         "forwardConfig": {
           "targetGroups": [
-            {"serviceName": "app-v1", "servicePort": 80, "weight": $((100 - NEW_WEIGHT))},
-            {"serviceName": "$SERVICE", "servicePort": 80, "weight": $NEW_WEIGHT}
+            { "serviceName": "app-v1", "servicePort": 80, "weight": $OLD_WEIGHT },
+            { "serviceName": "$SERVICE", "servicePort": 80, "weight": $NEW_WEIGHT }
           ]
         }
       }
@@ -31,7 +34,9 @@ spec:
             pathType: Prefix
             backend:
               service:
-                name: weighted-canary  # <-- This is a virtual service (matches the annotation)
+                name: canary
                 port:
-                  name: use-annotation  # <-- Must literally be this string
-EOF
+                  name: use-annotation
+EOM
+
+
